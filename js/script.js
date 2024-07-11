f8e31a972d380f4707b2d6ee38c3bd4d0ecee37b
David <david.jean.reis23@gmail.com>
2024-07-11 18:10:16 -0300

    Adicionei links aos meus cards, ajuste nas imagens

--- js/script.js
+++ js/script.js
@@ -8,4 +8,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
-  
+const navLinks = document.querySelectorAll('.navigation-links a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
