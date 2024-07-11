9d84bed2dac01e6c55a70ab50bcb34a97000fc94
David <david.jean.reis23@gmail.com>
2024-07-11 17:18:06 -0300

    Adicionei menu hamburguer responsivo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+  
+    menuIcon.addEventListener('click', function() {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
+  
