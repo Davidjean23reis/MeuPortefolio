03936d5ddec2043c236135e5f8e2819c6c2b61b9
David <david.jean.reis23@gmail.com>
2024-07-12 01:43:51 -0300

    Implementação de animação, Ajuste do menu  hamburgue

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.classList.add('loaded');
+});
 
 document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav-links');
-  
-    menuIcon.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
-    });
+  const menuIcon = document.getElementById('menu-icon');
+  const navLinks = document.getElementById('nav-links');
+  const navLinksItems = document.querySelectorAll('.navigation-links a');
+
+  menuIcon.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
   });
-  
-const navLinks = document.querySelectorAll('.navigation-links a');
 
-navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
+  navLinksItems.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
 
-    const targetId = this.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-
-    if (targetSection) {
-      targetSection.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-    }
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+        navLinks.classList.remove('active'); 
+      }
+    });
   });
 });
